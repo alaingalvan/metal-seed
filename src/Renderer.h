4418a27c9752d09a0287c03f78f750c97fbeae51
Alain Galvan <hi@alain.xyz>
2019-09-18 17:52:06 -0400

    Added source files

---
+++ src/Renderer.h
@@ -0,0 +1,158 @@
+#pragma once
+
+#include "CrossWindow/CrossWindow.h"
+#include "CrossWindow/Graphics.h"
+
+#define GLM_FORCE_SSE42 1
+#define GLM_FORCE_DEFAULT_ALIGNED_GENTYPES 1
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
+#include <vector>
+#include <chrono>
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+
+#include <unistd.h>
+
+// Common Utils
+
+inline std::vector<char> readFile(const std::string& filename) {
+	std::string path = filename;
+	char pBuf[1024];
+#ifdef XWIN_WIN32
+
+	_getcwd(pBuf, 1024);
+	path = pBuf;
+	path += "\\";
+#else
+	getcwd(pBuf, 1024);
+	path = pBuf;
+	path += "/";
+#endif
+	path += filename;
+	std::ifstream file(path, std::ios::ate | std::ios::binary);
+	bool exists = (bool)file;
+
+	if (!exists || !file.is_open()) {
+		throw std::runtime_error("failed to open file!");
+	}
+
+	size_t fileSize = (size_t)file.tellg();
+	std::vector<char> buffer(fileSize);
+
+	file.seekg(0);
+	file.read(buffer.data(), fileSize);
+
+	file.close();
+
+	return buffer;
+};
+
+template <typename T>
+inline T clamp(const T& value, const T& low, const T& high)
+{
+	return value < low ? low : (value > high ? high : value);
+}
+
+
+// Renderer
+
+class Renderer
+{
+public:
+	Renderer(xwin::Window& window);
+
+	~Renderer();
+
+	// Render onto the render target
+	void render();
+
+	// Resize the window and internal data structures
+	void resize(unsigned width, unsigned height);
+
+protected:
+
+	// Initialize your Graphics API
+	void initializeAPI(xwin::Window& window);
+
+	// Destroy any Graphics API data structures used in this example
+	void destroyAPI();
+
+	// Initialize any resources such as VBOs, IBOs, used in this example
+	void initializeResources();
+
+	// Destroy any resources used in this example
+	void destroyResources();
+
+	// Create graphics API specific data structures to send commands to the GPU
+	void createCommands();
+
+	// Set up commands used when rendering frame by this app
+	void setupCommands();
+
+	// Destroy all commands
+	void destroyCommands();
+
+	// Set up the FrameBuffer
+	void initFrameBuffer();
+
+	void destroyFrameBuffer();
+
+	// Set up the RenderPass
+	void createRenderPass();
+
+	void createSynchronization();
+
+	// Set up the swapchain
+	void setupSwapchain(unsigned width, unsigned height);
+
+	struct Vertex
+	{
+		float position[3];
+		float color[3];
+	};
+
+	Vertex mVertexBufferData[3] =
+	{
+	  { { 1.0f,  1.0f, 0.0f },{ 1.0f, 0.0f, 0.0f } },
+	  { { -1.0f,  1.0f, 0.0f },{ 0.0f, 1.0f, 0.0f } },
+	  { { 0.0f, -1.0f, 0.0f },{ 0.0f, 0.0f, 1.0f } }
+	};
+
+	uint32_t mIndexBufferData[3] = { 0, 1, 2 };
+
+	std::chrono::time_point<std::chrono::steady_clock> tStart, tEnd;
+	float mElapsedTime = 0.0f;
+
+	// Uniform data
+	struct {
+		glm::mat4 projectionMatrix;
+		glm::mat4 modelMatrix;
+		glm::mat4 viewMatrix;
+	} uboVS;
+
+	// The device (aka GPU) we're using to render
+	id<MTLCommandBuffer> mDevice;
+
+	CAMetalLayer* mLayer;
+	// The command Queue from which we'll obtain command buffers
+	id<MTLCommandQueue> mCommandQueue;
+
+	// The current size of our view so we can use this in our render pipeline
+	unsigned mViewportSize[2];
+
+	//Resources
+	id<MTLLibrary> vertLibrary;
+	id<MTLLibrary> fragLibrary;
+	id<MTLFunction> vertexFunction;
+	id<MTLFunction> fragmentFunction;
+	id<MTLBuffer> mVertexBuffer;
+	id<MTLBuffer> mIndexBuffer;
+	id<MTLBuffer> mUniformBuffer;
+	id<MTLRenderPipelineState> mPipelineState;
+	id<MTLCommandBuffer> mCommandBuffer;
+
+
+};
---
+++ src/Renderer.mm
@@ -0,0 +1,240 @@
+#include "Renderer.h"
+#import <Metal/Metal.h>
+#import <QuartzCore/CAMetalLayer.h>
+
+Renderer::Renderer(xwin::Window& window)
+{
+	initializeAPI(window);
+	initializeResources();
+	setupCommands();
+	tStart = std::chrono::high_resolution_clock::now();
+}
+
+Renderer::~Renderer()
+{
+	destroyResources();
+	destroyAPI();
+}
+
+void Renderer::initializeAPI(xwin::Window& window)
+{
+	xgfx::createMetalLayer(&window);
+	xwin::WindowDelegate& del = window.getDelegate();
+	CAMetalLayer* layer = (CAMetalLayer*)del.layer;
+	mLayer = layer;
+	
+	layer.device = MTLCreateSystemDefaultDevice();
+	mDevice = layer.device;
+	
+	// Create the command queue
+	mCommandQueue = [mDevice newCommandQueue];
+	
+	xwin::WindowDesc desc = window.getDesc();
+	
+	mViewportSize[0] = desc.width;
+	mViewportSize[1] = desc.height;
+}
+
+void Renderer::destroyAPI()
+{
+	if (mCommandBuffer != nil)
+	{
+		[mCommandBuffer release];
+	}
+	
+	[mCommandQueue release];
+	
+	[mDevice release];
+}
+
+void Renderer::initializeResources()
+{
+	// Create Vertex Buffer
+	
+	mVertexBuffer = [mDevice newBufferWithLength:sizeof(Vertex) * 3
+														options:MTLResourceOptionCPUCacheModeDefault];
+	[mVertexBuffer setLabel:@"VBO"];
+	memcpy(mVertexBuffer.contents, mVertexBufferData, sizeof(Vertex) * 3);
+	
+	// Create Index Buffer
+	
+	mIndexBuffer = [mDevice newBufferWithLength:sizeof(unsigned) * 3
+													   options:MTLResourceOptionCPUCacheModeDefault];
+	[mIndexBuffer setLabel:@"IBO"];
+	memcpy(mIndexBuffer.contents, mIndexBufferData, sizeof(unsigned) * 3);
+	
+	// Create Uniform Buffer
+	mUniformBuffer = [mDevice newBufferWithLength:(sizeof(uboVS) + 255) & ~255
+														 options:MTLResourceOptionCPUCacheModeDefault];
+	[mUniformBuffer setLabel:@"UBO"];
+	
+	// Update Uniforms
+	float zoom = -2.5f;
+	
+	// Update matrices
+	uboVS.projectionMatrix = Matrix4::perspective(45.0f, (float)mViewportSize[0] / (float)mViewportSize[1], 0.01f, 1024.0f);
+	
+	uboVS.viewMatrix = Matrix4::translation(Vector3(0.0f, 0.0f, zoom)) * Matrix4::rotationZ(3.14f);
+	
+	uboVS.modelMatrix = Matrix4::identity();
+	
+	size_t uboSize = sizeof(uboVS);
+	
+	memcpy((mUniformBuffer).contents, &uboVS, uboSize);
+	
+	// Load all the shader files with a .msl file extension in the project
+	NSError* err = nil;
+	
+	// Load shader files, add null terminator to the end.
+	std::vector<char> vertSource = readFile("triangle.vert.msl");
+	vertSource.emplace_back(0);
+	std::vector<char> fragSource = readFile("triangle.frag.msl");
+	fragSource.emplace_back(0);
+	
+	{
+		NSString* vertPath = [NSString stringWithCString:vertSource.data() encoding:[NSString defaultCStringEncoding]];
+		id<MTLLibrary> vLibrary = [mDevice newLibraryWithSource:vertPath options:nil error:&err];
+		vertLibrary = vLibrary;
+	}
+	
+	{
+		NSString* fragPath = [NSString stringWithCString:fragSource.data() encoding:[NSString defaultCStringEncoding]];
+		id<MTLLibrary> fLibrary = [mDevice newLibraryWithSource:fragPath options:nil error:&err];
+		fragLibrary = fLibrary;
+	}
+	
+	// Load the vertex function from the library
+	vertexFunction = [vertLibrary newFunctionWithName:@"main0"];
+	
+	// Load the fragment function from the library
+	fragmentFunction = [fragLibrary newFunctionWithName:@"main0"];
+	
+	// Configure a pipeline descriptor that is used to create a pipeline state
+	MTLRenderPipelineDescriptor* pipelineStateDescriptor = [[MTLRenderPipelineDescriptor alloc] init];
+	pipelineStateDescriptor.label = @"Simple Pipeline";
+	pipelineStateDescriptor.vertexFunction = vertexFunction;
+	pipelineStateDescriptor.fragmentFunction = fragmentFunction;
+	pipelineStateDescriptor.colorAttachments[0].pixelFormat = ((CAMetalLayer*)mLayer).pixelFormat;
+	
+	MTLVertexDescriptor* vertexDesc = [MTLVertexDescriptor vertexDescriptor];
+	vertexDesc.attributes[0].format = MTLVertexFormatFloat3;
+	vertexDesc.attributes[0].offset = 0;
+	vertexDesc.attributes[0].bufferIndex = 0;
+	vertexDesc.attributes[1].format = MTLVertexFormatFloat3;
+	vertexDesc.attributes[1].offset = sizeof(float) * 3;
+	vertexDesc.attributes[1].bufferIndex = 0;
+	vertexDesc.layouts[0].stepFunction = MTLVertexStepFunctionPerVertex;
+	vertexDesc.layouts[0].stride = sizeof(Vertex);
+	
+	pipelineStateDescriptor.vertexDescriptor = vertexDesc;
+	NSError* error = nil;
+	
+	mPipelineState = [mDevice newRenderPipelineStateWithDescriptor:pipelineStateDescriptor
+																			error:&error];
+	if (!mPipelineState)
+	{
+		// Pipeline State creation could fail if we haven't properly set up our pipeline descriptor.
+		//  If the Metal API validation is enabled, we can find out more information about what
+		//  went wrong.  (Metal API validation is enabled by default when a debug build is run
+		//  from Xcode)
+		NSLog(@"Failed to created pipeline state, error %@", error);
+		return;
+	}
+	
+}
+
+void Renderer::destroyResources()
+{
+	[fragmentFunction release];
+	[vertexFunction release];
+	
+	[vertLibrary release];
+	[fragLibrary release];
+	
+	[mVertexBuffer release];
+	[mIndexBuffer release];
+	[mUniformBuffer release];
+	
+	[mPipelineState release];
+}
+
+void Renderer::resize(unsigned int width, unsigned int height)
+{
+	mViewportSize[0] = width;
+	mViewportSize[1] = height;
+}
+
+void Renderer::setupCommands()
+{
+	// Commands are set at render time
+}
+
+void Renderer::render()
+{
+	// Framelimit set to 60 fps
+	tEnd = std::chrono::high_resolution_clock::now();
+	float time = std::chrono::duration<float, std::milli>(tEnd - tStart).count();
+	if (time < (1000.0f / 60.0f))
+	{ return; }
+	tStart = std::chrono::high_resolution_clock::now();
+	
+	// Update uniforms
+	
+	mElapsedTime += 0.001f * time;
+	mElapsedTime = fmodf(mElapsedTime, 6.283185307179586f);
+	
+	uboVS.modelMatrix = Matrix4::rotationY(mElapsedTime);
+	memcpy((mUniformBuffer).contents, &uboVS, sizeof(uboVS));
+	
+	// Create a new command buffer for each render pass to the current drawable
+	
+	if (mCommandBuffer != nil)
+	{ [mCommandBuffer release]; }
+	mCommandBuffer = [mCommandQueue commandBuffer];
+	mCommandBuffer.label = @"MyCommand";
+	
+	// Build renderPassDescriptor generated from the view's drawable textures
+	id<CAMetalDrawable> drawable = mLayer.nextDrawable;
+	
+	MTLRenderPassDescriptor* renderPassDescriptor = [MTLRenderPassDescriptor renderPassDescriptor];
+	renderPassDescriptor.colorAttachments[0].texture = drawable.texture;
+	renderPassDescriptor.colorAttachments[0].loadAction = MTLLoadActionClear;
+	
+	MTLClearColor clearCol;
+	clearCol.red = 0.2;
+	clearCol.green = 0.2;
+	clearCol.blue = 0.2;
+	clearCol.alpha = 1.0;
+	renderPassDescriptor.colorAttachments[0].clearColor = clearCol;
+	
+	if(renderPassDescriptor != nil)
+	{
+		// Create a render command encoder so we can render into something
+		id<MTLRenderCommandEncoder> renderEncoder =
+		[(mCommandBuffer renderCommandEncoderWithDescriptor:renderPassDescriptor];
+		renderEncoder.label = @"MyRenderEncoder";
+		
+		// Set the region of the drawable to which we'll draw.
+		[renderEncoder setViewport:(MTLViewport){0.0, 0.0, static_cast<float>(mViewportSize[0]), static_cast<float>(mViewportSize[1]), 0.1, 1000.0 }];
+		
+		[renderEncoder setRenderPipelineState: mPipelineState];
+		
+		[renderEncoder setCullMode:MTLCullModeNone];
+		
+		[renderEncoder setVertexBuffer:mUniformBuffer offset:0 atIndex:1];
+		
+		[renderEncoder setVertexBuffer:mVertexBuffer offset:0 atIndex:0];
+		
+		
+		[renderEncoder drawIndexedPrimitives:MTLPrimitiveTypeTriangle indexCount:3 indexType:MTLIndexTypeUInt32 indexBuffer:mIndexBuffer indexBufferOffset:0];
+		
+		[renderEncoder endEncoding];
+		
+		[mCommandBuffer presentDrawable:drawable];
+		
+		[mCommandBuffer commit];
+		
+		
+	}
+	
+}
